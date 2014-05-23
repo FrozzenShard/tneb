71e073a05a2c587d5c0671df4ba9ecb57af4dd72
robotmayo <robotmayo@gmail.com>
2014-05-23 06:32:01 -0400

    Logger, now working!

--- src/js/tneb/logger.js
+++ src/js/tneb/logger.js
@@ -1,9 +1,36 @@
 (function(){
     var root = this;
-    var logger = {};
+    var $ = require('jQuery');
+    var GameLogger = {};
+    var el;
+    GameLogger.displayList = [];
+    GameLogger.currentMessages = [];
+    GameLogger.fullLog = [];
+    GameLogger.el = $("#logger");
+    GameLogger.maxMessages = 3;
+    el = GameLogger.el;
+    GameLogger.log = function(type,message){
+        var curTime = Date.now(),
+            len;
+        this.fullLog.push({message : message, timestamp : curTime, type : type});
+        this.currentMessages.push({message : message, timestamp : curTime, type : type});
+        if(this.currentMessages.length > this.maxMessages){
+            this.currentMessages.splice(0,1);
+            console.log(this.currentMessages.length,this.currentMessages);
+        }
+        len = this.currentMessages.length;
+        while(len--){
+            this.displayList[len].innerHTML = this.currentMessages[len].message;
+        }
+    };
+    for(var i = 0; i < GameLogger.maxMessages; i++){
+        var p = document.createElement('p');
+        GameLogger.displayList.push(p);
+        el.append(p);
+    }
     
     if(typeof module !== 'undefined' && module.exports){
-        module.exports = logger;
+        module.exports = GameLogger;
     }
-    root.logger = logger;
+    root.GameLogger = GameLogger;
 }());
